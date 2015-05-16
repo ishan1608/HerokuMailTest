0d6615da55083517ceec9f0d7ee7992c033f4399
Amarjeet Kumar <amarjeet.0804kumar@gmail.com>
2015-05-16 21:47:58 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+var http = require('http');
+
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport();
+
+http.createServer(function(req, res) {
+	console.log('Request came');
+
+	var transporter = nodemailer.createTransport({
+	  service: 'Hotmail',
+	      auth: {
+	          user: 'tempexp@outlook.com',
+	          pass: 'm7382in@01'
+	      }
+	  });
+	   transporter.sendMail({
+	      from: 'tempexp@outlook.com',
+	      to: 'ishan1608@live.com',
+	      subject: 'Mail using heroku',
+	      text: 'This mail is send using nodemailer on heroku.',
+	      html: '<p>This mail is send using <b>nodemailer</b> on <b>heroku</b></p>'
+	  }, function(err, response){
+			console.log('Mail sending done :');
+			console.dir(err);
+			console.dir(response);
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			if (err) {
+				res.end('Mail not sent\n\nError :\n' + JSON.stringify(err) + '\n\nResponse :\n' + JSON.stringify(response));
+			} else {
+				res.end('Mail sent successfully\n\nError :\n' + JSON.stringify(err) + '\n\nResponse :\n' + JSON.stringify(response));
+			}
+	  });
+}).listen(Number(process.env.PORT || 8080));
+console.log('Server started at port ' + Number(process.env.PORT || 8080));
